1ae8f1db7cd7e4c3480897449129bf40a009cc92
Marcus Simonsen <marcuss@pressreader.com>
2025-03-26 16:04:34 -0700

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "main" {
-  value = vault_kv_secret_v2.secret.id
+
+  value = {
+    id       = vault_kv_secret_v2.secret.id
+    path     = vault_kv_secret_v2.secret.name
+    metadata = vault_kv_secret_v2.secret.custom_metadata
+  }
 }
